8640cccce8b276eec344024103efb35384b7badb
looboolife <looboolife@163.com>
2020-10-17 15:15:31 +0800

    move *.sh to dir

--- 3_export.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "You are welcome to use bash"
-echo "Current work directory is $PWD"
-echo "the host home dir is $HOSTNAME"
-echo "your home dir $HOME"
-echo "your shell is $SHELL"
-echo "user env val is $MYVAL"
